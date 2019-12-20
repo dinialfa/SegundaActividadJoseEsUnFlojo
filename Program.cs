8627872ff2be34fa63fc993eb2597454fa6f33ac
Daniel Gonzalez Garcia <dinig28@gmail.com>
2019-12-20 09:56:10 +0100

    esto es el script a cambiar

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JoseEsUnFlojo2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Jose se cree que tiene idea de filosofia pero no tiene ni idea");
+        }
+    }
+}
